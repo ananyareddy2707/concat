4252a87aa9f94bed0889bb153dacf21ac4155ffc
ananyareddy2707 <115121894+ananyareddy2707@users.noreply.github.com>
2022-12-01 14:35:07 -0500

    concat

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;  
+
+namespace Program 
+{  
+    class Program 
+    {
+        public static void Main(string [] args) 
+        {
+            string s1 = "My age is";
+            string s2 = " 34";
+            string s3=string.Concat(s1, s2);
+            Console.WriteLine(s3);
+        }
+    } 
+}
+
